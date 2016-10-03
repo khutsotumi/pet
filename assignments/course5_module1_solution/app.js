a46e1ec09af36446742ff9135c25b1495dc1c9a4
Oupa Mashile <oupamashile@gmail.com>
2016-10-03 23:47:03 +0200

    fixed IIFE syntax

--- assignments/course5_module1_solution/app.js
+++ assignments/course5_module1_solution/app.js
@@ -1,63 +1,31 @@
 (function () {
-'use strict';
-
-angular.module('LunchCheck', [])
-.controller('LunchCheckController', LunchCheckController)
-// .filter('input', inputFilter)
-// .filter('message', messageFilter);
-;
-//LunchCheckController.$inject = ['$scope', 'inputFilter'];
-LunchCheckController.$inject = ['$scope', '$filter'];
-// function LunchCheckController($scope, inputFilter) {
-function LunchCheckController($scope, $filter) {
-  //get user input
-  $scope.name = "";
-  $scope.calcItems = getItems($scope.name);
-  $scope.message = messageFilter();
-
-  //$scope.stateOfBeing = "hungry";
-
-  // $scope.sayMessage = function () {
-  //   var msg = "Yaakov likes to eat healthy snacks at night!";
-  //   return msg;
-  // };
-  //
-  // $scope.sayLovesMessage = function () {
-  //   var msg = "Yaakov likes to eat healthy snacks at night!";
-  //   msg = lovesFilter(msg)
-  //   return msg;
-  // };
-
-  // $scope.feedYaakov = function () {
-  //   $scope.stateOfBeing = "fed";
-  // };
-};
-
-function getItems(string) {
-  var numItems = string.split(",");
-  // for (var i = 0; i < string.length; i++) {
-  //   if (string.charAt(i)===",") {
-  //     numItems++;
-  //   }
-  // }
-  message = "numItems";
-  return numItems;
-};
-
-function messageFilter() {
-  // return function (input, target, replace) {
-  //   input = input || "";
-  //   input = input.replace(target, replace);
-  //   return input;
-  // }
-  if (calcItems<4) {
-      message = "Enjoy";
-  } else if (calcItems > 3){
-    message = "Too much";
-  }
-  else {
-    message = "you must add a list of items in text box";
-  }
-};
-
+    'use strict';
+    angular.module('LunchCheck', [])
+    .controller('LunchCheckController', LunchCheckController)
+    // .filter('input', inputFilter)
+    .filter('message', messageFilter);
+    //LunchCheckController.$inject = ['$scope', 'inputFilter'];
+    LunchCheckController.$inject = ['$scope', '$filter'];
+    // function LunchCheckController($scope, inputFilter) {
+    function LunchCheckController($scope, $filter) {
+      $scope.name = "";
+      $scope.calcItems = function() {
+        var str = $scope.name;
+        var numItems = str.split(",");
+        return numItems;
+      } ;
+      $scope.message = function() {
+        var msg = "";
+        var words = calcItems();
+        if (words < 4) {
+          msg = "Enjoy";
+        } else if (words > 3){
+          msg = "Too much";
+        }
+        else {
+          msg = "you must add a list of items in text box";
+        }
+        return msg;
+      } ;
+    }; //end of function LunchCheckController
 })();
