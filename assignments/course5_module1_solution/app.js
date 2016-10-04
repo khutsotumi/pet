ef9d4fc008c422310cc2ce8314483d866d8e31f4
Oupa Mashile <oupamashile@gmail.com>
2016-10-04 21:15:12 +0200

    Revisions based on other students' input

--- assignments/course5_module1_solution/app.js
+++ assignments/course5_module1_solution/app.js
@@ -2,32 +2,43 @@
     'use strict';
     angular.module('LunchCheck', [])
     .controller('LunchCheckController', LunchCheckController);
-    // .filter('input', inputFilter)
-    //.filter('message', messageFilter);
-    //LunchCheckController.$inject = ['$scope', 'inputFilter'];
     LunchCheckController.$inject = ['$scope'];
-    // function LunchCheckController($scope, inputFilter) {
     function LunchCheckController($scope) {
-      $scope.name;
-      $scope.calcItems = function() {
-        var str = $scope.name;
-        var numItems = str.split(",");
-        return numItems.length;
-      } ;
-      $scope.message = function() {
-        var msg = "";
-        var words = $scope.calcItems();
-        console.log("number of words is: " + words);
+      $scope.lunchMenu = "";
+      $scope.checkOutput="";
 
-        if (words < 4) {
-          msg = "Enjoy";
-        } else if (words > 3){
-          msg = "Too much";
+      $scope.calcItems = function () {
+        var numberOfItems = $scope.lunchMenu.split(',').length;
+        if ($scope.lunchMenu == "") {
+          $scope.checkOutput = "Please enter data first";
+        }
+        else if (numberOfItems <= 3) {
+          $scope.checkOutput = "Enjoy!";
         }
         else {
-          msg = "you must add a list of items in text box";
+          $scope.checkOutput = "Too much!";
         }
-        return msg;
-      } ;
-    }; //end of function LunchCheckController
+      };
+
+      //the code below was my original and I found the above from a student on coursera
+      // $scope.calcItems = function() {
+      //   var str = $scope.name;
+      //   var numItems = str.split(",");
+      //   return numItems.length;
+      // } ;
+
+      // $scope.message = function() {
+      //   var msg = "";
+      //   var words = $scope.calcItems();
+      //   if (words < 4) {
+      //     msg = "Enjoy";
+      //   } else if (words > 3){
+      //     msg = "Too much";
+      //   }
+      //   else {
+      //     msg = "you must add a list of items in text box";
+      //   }
+      //   return msg;
+      // } ;
+    };
 })();
